752622871b3285ef8a055d6c185f943abd16e8f2
Jackie Muzakki <jackie.muzakki@gmail.com>
2025-05-27 15:44:32 +0700

    fix(handler): log errors when rendering static files

--- pkg/handler/static.go
+++ pkg/handler/static.go
@@ -1,13 +1,16 @@
 package handler
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 type RenderStatic func(w http.ResponseWriter, r *http.Request) error
 
 func HandleStatic(renderer RenderStatic) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if err := renderer(w, r); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+			log.Println("error rendering static file:", err)
 		}
 	}
 }
